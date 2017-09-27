f1f381587d110a0f0e4ffd6d924665de778ca0cf
Bill Donovan <williamcdonovan@gmail.com>
2017-09-27 16:53:25 -0600

    Set up handlebars for HTML templating

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 //  Dependencies
 // ==========================================
 const express = require('express');
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
 
 var PORT = process.env.PORT || 3000;
 
@@ -13,6 +15,18 @@ const router = express.Router();
 
 //  Designated public folder as a static directory
 app.use(express.static(__dirname + '/public'));
+
+//  Use handlebars for HTML templating
+app.engine('handlebars', expressHandlebars({
+	defaultLayout: 'main'
+}));
+
+app.set("view engine", "handlebars");
+
+//  Use body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(router);
 
 app.listen(PORT, ()=>
 	console.log('Listening on port: ' + PORT));
